4789ffbc6d0b4f3df0964ec5adc2ff09d779c935
Ruslan <rdantsov@tula.co>
2025-06-23 01:13:38 +0300

    fix server host address env name

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Config struct {
-	Address            string `short:"a" long:"address" env:"ADDRESS" default:"localhost:8090" description:"Server host address"`
+	Address            string `short:"a" long:"address" env:"RUN_ADDRESS" default:"localhost:8090" description:"Server host address"`
 	LogLevel           string `short:"l" long:"log" env:"LOG_LEVEL" default:"INFO" description:"Log Level"`
 	DatabaseConnection string `short:"d" long:"database" env:"DATABASE_URI" description:"Database connection string"`
 	JWTSecret          string `short:"j" long:"jwt" env:"JWT_SECRET" default:"rabbit_Hole" description:"Database connection string"`
