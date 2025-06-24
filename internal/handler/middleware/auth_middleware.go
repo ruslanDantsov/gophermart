1c92f95aea2737eb123876cfeca1fa9e3a802f2b
Ruslan <rdantsov@tula.co>
2025-06-24 12:37:02 +0300

    add more logs to auth middleware

--- internal/handler/middleware/auth_middleware.go
+++ internal/handler/middleware/auth_middleware.go
@@ -15,15 +15,25 @@ type CtxUserIDKey struct{}
 
 func AuthMiddleware(jwtSecret string, logger *zap.Logger) gin.HandlerFunc {
 	return func(gContext *gin.Context) {
+		var tokenString string
 		authHeader := gContext.GetHeader("Authorization")
-		if len(authHeader) < 7 || authHeader[:7] != "Bearer " {
-			logger.Error("Missing or invalid token in request")
-			gContext.JSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid token"})
-			gContext.Abort()
-			return
+		if len(authHeader) >= 7 && authHeader[:7] == "Bearer " {
+			tokenString = authHeader[7:]
+		} else {
+			if len(authHeader) > 0 {
+				logger.Error(fmt.Sprintf("Value of auth header: %s", authHeader))
+			}
+			// Если нет в заголовке, пробуем получить из cookie
+			cookie, err := gContext.Cookie("Authorization")
+			if err == nil && cookie != "" {
+				tokenString = cookie
+			} else {
+				logger.Error("Missing or invalid token in request")
+				gContext.JSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid token"})
+				gContext.Abort()
+				return
+			}
 		}
-
-		tokenString := authHeader[7:]
 
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
